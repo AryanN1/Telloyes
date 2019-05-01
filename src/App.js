7c7972fdeb28d84dd102bce9038b63338331ab5c
Aryan Naimi <aryannaimi@gmail.com>
2019-04-30 20:02:20 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import List from './List'
+import STORE from './STORE'
 import './App.css';
 
-class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
+const newRandomCard=()=> {
+  const id = Math.random().toString(36).substring(2,4)
+    Math.random().toString(36).substring(2,4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: "lorem ipsum"
     }
+}
+
+function omit(obj, ketToOmit){
+  return Object.entries(obj).reduce(
+    (newObj, [key,value])=>
+      key === keyToOmit ? newObj: {...newObj,[key]:
+value},
+{}
+);
+      }
+
+class App extends Component {
+  state = {
+    store: STORE,
   };
 
+  handleDeleteCard=(cardId) => {
+    const {lists,allCards} = this.state.store;
+
+    const newList = lists.map(list => ({
+      ...list,
+      cardIds: list.cardId.filter(id => id !=cardId)
+    }));
+
+    const newCards=omit(allCards, cardId);
+    this.setState({
+      store: {
+        lists: newList,
+        allCards: Newcards
+      }
+    })
+};
+
+  handleAddCard =(listId) => {
+    const newCard=newRandomCard()
+    const newLists = this.state.store.lists.map(list=> {
+      if(list.id ===listId){
+        return{
+          ...list,
+          cardId: [...list.cardId, newCard.id]
+        };
+      }
+      return list;
+    })
+this.setState({
+  store: {
+    lists: newList,
+    allCards: {
+      ...this.state.store.allCards,
+      [newCard.Id]: newCard
+    }
+  }
+})
+
+  };
+
+
+
+
+
   render() {
-    const { store } = this.props
+    const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
-          <h1>Trelloyes!</h1>
+     
         </header>
+        
         <div className='App-list'>
           {store.lists.map(list => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              onClickDelete={this.handleDeleteCard}
+              onClickAdd={this.handleAddCard}
             />
           ))}
         </div>
